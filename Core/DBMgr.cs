a4f2c92dfb00117cc13ed24343d6529800cd78eb
CK <chengkai.liang@bigfishgames.com>
2020-12-09 14:07:09 -0800

    update DB* namespace

--- Core/DBMgr.cs
+++ Core/DBMgr.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using System.Text;
 using System.Collections;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using Interop.SQLDMO;
 using System.Runtime.InteropServices;
 
